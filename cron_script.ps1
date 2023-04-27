32a0ae3bf448e2705fafb8db3d1f70f77a0974cc
joshhn <duy.huynhngoc.05@gmail.com>
2023-04-27 17:03:12 -0400

    Fix bugs

--- cron_script.ps1
+++ cron_script.ps1
@@ -1,5 +1,7 @@
 $cd ~/campuslabsChromeExtention/data-scraper
+$poetry install
 $poetry run python3 scraper.py
 $git add .
-$git commit -m "Update data automatically"
+$temp=$(date)
+$git commit -m "Auto update data at $temp"
 $git push origin main:daily-updates
