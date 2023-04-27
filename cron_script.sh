f83d51975e6f690446c9bc00114cc111974d50eb
joshhn <duy.huynhngoc.05@gmail.com>
2023-04-27 15:12:38 -0400

    Update data automatically

--- cron_script.sh
+++ cron_script.sh
@@ -1,4 +1,5 @@
-poetry run python3 data_scraper/scraper.py
+cd ~/Desktop/TigerHacks2023_DPU/data_scraper
+poetry run python3 scraper.py
 git add .
 git commit -m "Update data automatically"
 git push origin daily-updates
